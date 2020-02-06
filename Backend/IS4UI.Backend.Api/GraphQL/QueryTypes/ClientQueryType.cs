52f26365b9159274fa82749e6ed8665540cf3b35
Seb <seb@dfd.co.nz>
2020-02-06 16:56:21 +1300

    Populate ClientPostLogoutRedirectUris collection in Clients

--- Backend/IS4UI.Backend.Api/GraphQL/QueryTypes/ClientQueryType.cs
+++ Backend/IS4UI.Backend.Api/GraphQL/QueryTypes/ClientQueryType.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using HotChocolate;
 using HotChocolate.Types;
@@ -9,20 +10,48 @@ public class RootQueryType : ObjectType
 {
     protected override void Configure(IObjectTypeDescriptor descriptor)
     {
-        descriptor.Field<ClientsResolvers>(x => x.GetClients()).Type<ListType<NonNullType<ClientType>>>();
+        descriptor.Field<ClientResolvers>(x => x.GetClients()).Type<ListType<NonNullType<ClientType>>>();
+        descriptor.Field<ClientResolvers>(x => x.GetClient(default)).Type<NonNullType<ClientType>>().Argument("id", a => a.Type<IntType>());
     }
 }
 
 public class ClientType : ObjectType<Client>
 {
+    public ClientType()
+    {
+
+    }
+
+    protected override void Configure(IObjectTypeDescriptor<Client> descriptor)
+    {
+        descriptor.Field<ClientPostLogoutRedirectUriResolvers>(x => x.GetClientPostLogoutRedirectUrisByClientIdFromParent(default))
+                  .Type<ListType<NonNullType<ClientPostLogoutRedirectUriType>>>()
+                  .Name("clientPostLogoutRedirectUris");
+
+        // Another approach without magic string:
+        // descriptor.Field(x => x.ClientPostLogoutRedirectUris)
+        //           .Type<ListType<NonNullType<ClientPostLogoutRedirectUriType>>>()
+        //           .Resolver(context =>
+        //          {
+        //              var resolver = context.Resolver<ClientPostLogoutRedirectUriResolvers>();
+        //              var clientId = context.Parent<Client>().Id;
+        //              var result = resolver.GetClientPostLogoutRedirectUrisByClientId(clientId);
+        //              return result;
+        //          });
+
+    }
+}
+
+public class ClientPostLogoutRedirectUriType : ObjectType<ClientPostLogoutRedirectUri>
+{
 
 }
 
-public class ClientsResolvers
+public class ClientResolvers
 {
     private readonly ApplicationDbContext db;
 
-    public ClientsResolvers(ApplicationDbContext db)
+    public ClientResolvers(ApplicationDbContext db)
     {
         this.db = db;
     }
@@ -31,4 +60,43 @@ public class ClientsResolvers
     {
         return db.Clients.ToList();
     }
+
+    public Client GetClient(int id)
+    {
+        return db.Clients.SingleOrDefault(c => c.Id == id);
+    }
+
+}
+
+
+public class ClientPostLogoutRedirectUriResolvers
+{
+
+    private readonly ApplicationDbContext db;
+
+    public ClientPostLogoutRedirectUriResolvers(ApplicationDbContext db)
+    {
+        this.db = db;
+    }
+
+    public List<ClientPostLogoutRedirectUri> GetClientPostLogoutRedirectUris()
+    {
+        return db.ClientPostLogoutRedirectUris.ToList();
+    }
+
+    public List<ClientPostLogoutRedirectUri> GetClientPostLogoutRedirectUrisByClientId(int clientId)
+    {
+        return db.ClientPostLogoutRedirectUris.Where(x => x.ClientId == clientId).ToList();
+    }
+
+    public List<ClientPostLogoutRedirectUri> GetClientPostLogoutRedirectUrisByClientIdFromParent([Parent]Client client)
+    {
+        return db.ClientPostLogoutRedirectUris.Where(x => x.ClientId == client.Id).ToList();
+    }
+
+    public ClientPostLogoutRedirectUri GetClientPostLogoutRedirectUri(int id)
+    {
+        return db.ClientPostLogoutRedirectUris.SingleOrDefault(x => x.Id == id);
+    }
+
 }
