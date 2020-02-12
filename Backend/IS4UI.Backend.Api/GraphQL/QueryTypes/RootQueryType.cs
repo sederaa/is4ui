32d58412e6ae3d7de60d52838988a21819044a7d
Seb <seb@dfd.co.nz>
2020-02-12 17:37:47 +1300

    Move DbContext injection to methods

--- Backend/IS4UI.Backend.Api/GraphQL/QueryTypes/RootQueryType.cs
+++ Backend/IS4UI.Backend.Api/GraphQL/QueryTypes/RootQueryType.cs
@@ -4,7 +4,10 @@ public class RootQueryType : ObjectType
 {
     protected override void Configure(IObjectTypeDescriptor descriptor)
     {
-        descriptor.Field<ClientResolvers>(x => x.GetClients()).Type<ListType<NonNullType<ClientType>>>();
-        descriptor.Field<ClientResolvers>(x => x.GetClient(default)).Type<NonNullType<ClientType>>().Argument("id", a => a.Type<IntType>());
+        descriptor.Field<ClientResolvers>(x => x.GetClients(default))
+            .Type<ListType<NonNullType<ClientType>>>();
+        descriptor.Field<ClientResolvers>(x => x.GetClient(default, default))
+            .Type<NonNullType<ClientType>>()
+            .Argument("id", a => a.Type<IntType>());
     }
 }
--- Backend/IS4UI.Backend.Api/GraphQL/Resolvers/ClientResolvers.cs
+++ Backend/IS4UI.Backend.Api/GraphQL/Resolvers/ClientResolvers.cs
@@ -2,22 +2,16 @@ using System.Collections.Generic;
 using System.Linq;
 using IS4UI.Backend.Data;
 using IS4UI.Backend.Data.Entities;
+using HotChocolate;
 
 public class ClientResolvers
 {
-    private readonly ApplicationDbContext db;
-
-    public ClientResolvers(ApplicationDbContext db)
-    {
-        this.db = db;
-    }
-
-    public List<Client> GetClients()
+    public List<Client> GetClients([Service] ApplicationDbContext db)
     {
         return db.Clients.ToList();
     }
 
-    public Client GetClient(int id)
+    public Client GetClient([Service] ApplicationDbContext db, int id)
     {
         return db.Clients.SingleOrDefault(c => c.Id == id);
     }
