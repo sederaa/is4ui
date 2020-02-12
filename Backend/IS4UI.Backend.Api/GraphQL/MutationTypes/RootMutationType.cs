7e99180eed90e84eb96e10ad4dc71a8f76dbd101
Seb <seb@dfd.co.nz>
2020-02-12 20:23:28 +1300

    Add updateClient mutation

--- Backend/IS4UI.Backend.Api/GraphQL/MutationTypes/RootMutationType.cs
+++ Backend/IS4UI.Backend.Api/GraphQL/MutationTypes/RootMutationType.cs
@@ -14,6 +14,10 @@ public class RootMutationType : ObjectType<Mutation>
         descriptor.Field(m => m.CreateClient(default, default))
             .Type<ClientType>()
             .Argument("input", a => a.Type<CreateClientInputType>());
+
+        descriptor.Field(m => m.UpdateClient(default, default))
+            .Type<ClientType>()
+            .Argument("input", a => a.Type<UpdateClientInputType>());
     }
 }
 
@@ -58,6 +62,15 @@ public class Mutation
         await db.SaveChangesAsync();
         return client;
     }
+
+    public async Task<Client> UpdateClient([Service] ApplicationDbContext db, UpdateClientInput input)
+    {
+        var client = await db.FindAsync<Client>(input.Id);
+        client.ClientId = input.ClientId;
+        client.ClientName = input.ClientName;
+        await db.SaveChangesAsync();
+        return client;
+    }
 }
 
 public class CreateClientInputType : InputObjectType<CreateClientInput>
@@ -67,18 +80,78 @@ public class CreateClientInputType : InputObjectType<CreateClientInput>
         base.Configure(descriptor);
 
         descriptor.Field(x => x.ClientId)
-        .Type<NonNullType<StringType>>();
+            .Type<NonNullType<StringType>>();
 
         descriptor.Field(x => x.ClientName)
-        .Type<NonNullType<StringType>>();
+            .Type<NonNullType<StringType>>();
+    }
+}
 
+public class UpdateClientInputType : InputObjectType<UpdateClientInput>
+{
+    protected override void Configure(IInputObjectTypeDescriptor<UpdateClientInput> descriptor)
+    {
+        base.Configure(descriptor);
 
+        descriptor.Field(x => x.ClientId)
+            .Type<NonNullType<StringType>>();
+
+        descriptor.Field(x => x.ClientName)
+            .Type<NonNullType<StringType>>();
     }
 }
 
 public class CreateClientInput
 {
     //public int Id { get; set; }        
+    //public bool Enabled { get; set; }
+    public string ClientId { get; set; }
+    // public string ProtocolType { get; set; }
+    // public bool RequireClientSecret { get; set; }
+    public string ClientName { get; set; }
+    // public string Description { get; set; }
+    // public string ClientUri { get; set; }
+    // public string LogoUri { get; set; }
+    // public bool RequireConsent { get; set; }
+    // public bool AllowRememberConsent { get; set; }
+    // public bool AlwaysIncludeUserClaimsInIdToken { get; set; }
+    // public bool RequirePkce { get; set; }
+    // public bool AllowPlainTextPkce { get; set; }
+    // public bool AllowAccessTokensViaBrowser { get; set; }
+    // public string FrontChannelLogoutUri { get; set; }
+    // public bool FrontChannelLogoutSessionRequired { get; set; }
+    // public string BackChannelLogoutUri { get; set; }
+    // public bool BackChannelLogoutSessionRequired { get; set; }
+    // public bool AllowOfflineAccess { get; set; }
+    // public int IdentityTokenLifetime { get; set; }
+    // public string AllowedIdentityTokenSigningAlgorithms { get; set; }
+    // public int AccessTokenLifetime { get; set; }
+    // public int AuthorizationCodeLifetime { get; set; }
+    // public int? ConsentLifetime { get; set; }
+    // public int AbsoluteRefreshTokenLifetime { get; set; }
+    // public int SlidingRefreshTokenLifetime { get; set; }
+    // public int RefreshTokenUsage { get; set; }
+    // public bool UpdateAccessTokenClaimsOnRefresh { get; set; }
+    // public int RefreshTokenExpiration { get; set; }
+    // public int AccessTokenType { get; set; }
+    // public bool EnableLocalLogin { get; set; }
+    // public bool IncludeJwtId { get; set; }
+    // public bool AlwaysSendClientClaims { get; set; }
+    // public string ClientClaimsPrefix { get; set; }
+    // public string PairWiseSubjectSalt { get; set; }
+    // public DateTime Created { get; set; }
+    // public DateTime? Updated { get; set; }
+    // public DateTime? LastAccessed { get; set; }
+    // public int? UserSsoLifetime { get; set; }
+    // public string UserCodeType { get; set; }
+    // public int DeviceCodeLifetime { get; set; }
+    // public bool NonEditable { get; set; }
+
+}
+
+public class UpdateClientInput
+{
+    public int Id { get; set; }
     //public bool Enabled { get; set; }
     public string ClientId { get; set; }
     // public string ProtocolType { get; set; }
